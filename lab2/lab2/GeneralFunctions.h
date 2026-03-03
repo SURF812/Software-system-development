b739f83a78c10bf7340e31b566496376a13a2607
SURF812 <georgbel888@gmail.com>
2026-03-03 18:25:31 +0300

    Add files via upload

---
+++ lab2/lab2/GeneralFunctions.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <vector>
+
+std::vector<int> selection_sort(const std::vector<int> vec);
+void save_vector(std::vector<int> vec, std::vector<int> result_vec, std::string url);
